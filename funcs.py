be7aa9d27a74de9ded03f822c9a828584c0c0083
Erick Tran <thelittlebotengineer@gmail.com>
2024-02-28 22:24:18 -0800

    Update 3

--- funcs.py
+++ funcs.py
@@ -1,23 +1,65 @@
 import binascii
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 from functools import reduce
+from PIL import Image
 
-def toBin(BASE, file, rwm):
-    with open(file, rwm) as file:
+
+def toBin(BASE, file_path, mode):
+    with open(file_path, mode) as file:
         # Read the file
         data = file.read()
         # Convert the file to hex
         hex_data = binascii.hexlify(data.encode())
+        # Convert hex to binary and return as a list of digits
+        return list(bin(int(hex_data, BASE))[2:])  # Start from index 2 to remove '0b' prefix
 
-        pixels = list(bin(int(hex_data, BASE)))
-
-        return pixels
-
-def toTxt(pixels, BASE, rwm, file):
-    with open(file, rwm) as file:
-        binaryArr = reduce(lambda x, y: x + y, pixels)
-        hex_data = hex(int(binaryArr, BASE))[4:]  # Convert binary to hexadecimal
+def toTxt(pixels, BASE, file_path, mode):
+    with open(file_path, mode) as file:
+        # Join binary digits into a string and convert to hexadecimal
+        hex_data = hex(int(''.join(map(str, pixels)), 2))
         # Decode hexadecimal data to text using UTF-8 encoding
-        text_data = binascii.unhexlify(hex_data).decode('utf-8')
+        text_data = binascii.unhexlify(hex_data[2:]).decode('utf-8')
         # Write the text data to the file
         file.write(text_data)
+
+
+def toImg(pixels, file):
+    if not isinstance(pixels, np.ndarray):
+        raise ValueError('Input must be a valid numpy array')
+
+    # Convert '1's to white and others to black
+    img = np.where(pixels == '1', 255, 0)
+
+    # Save the image to a file
+    plt.imsave(file, img)
+
+    # Display the image
+    # plt.imshow(img)
+    # plt.show()
+
+    return img
+
+def fillRemainingElements(pixels, frame_height, frame_width):
+    # Calculate the number of elements to fill
+    remaining_elements = frame_height * frame_width - pixels.size
+
+    # Fill the remaining elements with empty space
+    if remaining_elements > 0:
+        pixels = np.append(pixels, ['1'] * 8)
+        remaining_elements -= 8
+        pixels = np.append(pixels, ['0'] * remaining_elements)
+
+    return pixels
+
+
+def readImgToBin(file):
+    img = mpimg.imread(file)
+    # Convert the image to binary
+    npArray = np.array([])
+    for row in img:
+        npArray = np.append(npArray, row)
+
+    # Convert the binary array to a list
+    return npArray.tolist()
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import binascii
 import cv2
 import numpy as np
 from functools import reduce
-from funcs import toBin, toTxt
+from funcs import toBin, toTxt, toImg, fillRemainingElements, readImgToBin
 
 # Global constants
 BASE = 16
 FRAME_HEIGHT = 1080
 FRAME_WIDTH = 1920
+
 
 def main():
     frame_pixels = np.array([])
@@ -18,21 +19,29 @@ def main():
     print(len(pixels))
 
     # Open the file to write a hex file to text
-    toTxt(pixels, BASE, 'w', 'hex.txt')
+    # toTxt(pixels, BASE, 'hex.txt', 'w')
 
     frame_pixels = np.append(frame_pixels, pixels)
 
-    # Calculate the number of elements to fill
-    remaining_elements = FRAME_HEIGHT * FRAME_WIDTH - frame_pixels.size
-
     # Fill the remaining elements with empty space
-    if remaining_elements > 0:
-        frame_pixels = np.append(frame_pixels, ['0'] * remaining_elements)
-
+    frame_pixels = fillRemainingElements(frame_pixels, FRAME_HEIGHT,
+                                         FRAME_WIDTH)
     # Reshape the array to the frame dimensions
     frame_pixels = frame_pixels.reshape(FRAME_HEIGHT, FRAME_WIDTH)
 
-    print(frame_pixels.size)
+    # print(frame_pixels.size)
+
+    # Save the image to a file
+    img = toImg(frame_pixels, 'image.png')
+
+    # Read the image to binary
+    pixels = readImgToBin('image.png')
+
+    # Open the file to write a binary file to text
+    toTxt(pixels, BASE, 'output.txt', 'w')
+
+    exit(0)
+
 
 if __name__ == "__main__":
     main()
